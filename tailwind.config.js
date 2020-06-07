e44bce280836783197857887de0e5e52643b51d5
max <send2maks@gmail.com>
2020-06-07 20:45:09 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        '72':'18rem',
+        '84':'21rem',
+        '96':'24rem',
+        '108':'27rem',
+        '120':'30rem',
+        '132':'33rem',
+        '140':'34rem',
+        '142':'35rem',
+        '144':'36rem',
+        '156':'39rem',
+        '157':'41rem',
+        '158':'42rem',
+      },
+      maxWidth: {
+        'ss':'10rem',
+        'sss':'5rem',
+        'ssss':'3rem',
+        'sssss':'2rem',
+        'ssssss':'1rem',
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+        '9xl': '7rem',
+        '10xl': '8rem',
+        '11xl': '9rem',
+      },
+      padding: {
+        '22': '5.5rem',
+        '-100':'-100px'
+      },
+      inset: {
+        '-16':'-4rem',
+        '-20':'-5rem',
+        '-24':'-6rem',
+        '-32':'-8rem',
+        '-40':'-10rem',
+        '-30px':'-30px',
+        '-100px':'-100px',
+        '-140px':'-140px',
+        '-150px':'-150px',
+        '-160px':'-160px',
+        '16':'4rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
